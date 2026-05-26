50d4ee9674e15f5d4802b027946bee49b930955e
LeonPastorCarlosDaniel <U23211073@utp.edu.pe>
2026-05-26 16:57:57 -0500

    Se sube el backend para despliegue en Render.

---
+++ src/main/java/com/UTP/TpIntegrado/market/persistance/ProductoRepository.java
@@ -0,0 +1,74 @@
+package com.UTP.TpIntegrado.market.persistance;
+
+import com.UTP.TpIntegrado.market.domain.Product;
+import com.UTP.TpIntegrado.market.domain.repository.ProductRepository;
+import com.UTP.TpIntegrado.market.persistance.crud.ProductoCrudRepository;
+import com.UTP.TpIntegrado.market.persistance.entity.Producto;
+import com.UTP.TpIntegrado.market.persistance.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+
+
+    @Autowired
+    private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos  = (List<Producto>)
+                productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
+    public List<Producto> getByCategoria( int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+
+
+}
